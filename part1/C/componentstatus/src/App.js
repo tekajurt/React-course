1890fa40fc7b88a14c31168a40dcce6a2b71f7e7
tekajurt <tekajurt@gmail.com>
2021-08-19 00:41:03 -0400

    ejemplo c

---
+++ part1/C/componentstatus/src/App.js
@@ -0,0 +1,23 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+function App() {
+  const name = 'Peter'
+  const age = 10
+  return (
+    <div>
+      <h1>Greetins</h1>
+      <Hello name="Maya" age={26+10} />
+      <Hello name={name} age={age} />
+    </div>
+  );
+}
+
+export default App;
