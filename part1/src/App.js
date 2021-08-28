664a9c717dd54bcb5f83239a4926b9dbc7bfd968
tekajurt <tekajurt@gmail.com>
2021-08-28 11:48:00 -0400

    readme part1

---
+++ part1/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Display from './Display'
+import Button from './Boton'
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter +1)
+  const decreaseByOne = () => setCounter(counter -1)
+  const setToZero = () => setCounter(0)
+  
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
+  )
+}
+export default App;
