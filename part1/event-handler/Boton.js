04f6ce39ed23a7ea33c098775fcbac447c92eaad
tekajurt <tekajurt@gmail.com>
2021-08-28 12:44:09 -0400

    getting in complex state

--- part1/event-handler/Boton.js
+++ part1/event-handler/Boton.js
@@ -1,8 +1,6 @@
-const Button = ({ onClick, text }) => {
-  return (
-    <button onClick={onClick}>
-      {text}
-    </button>
-  )
-}
+const Button = ({ onClick, text }) =>  (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
 export default Button;
--- part1/event-handler/Display.js
+++ part1/event-handler/Display.js
@@ -1,6 +1,4 @@
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
-}
+const Display = ({counter}) =>  <div>{counter}</div>
+  
+
 export default Display;  
