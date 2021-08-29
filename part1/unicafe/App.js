d3de7713c3db230562167f1dd1de59afc8ce5d1c
tekajurt <tekajurt@gmail.com>
2021-08-29 15:17:25 -0400

    ex 1.8 done, y fix que el orden en que se apretaban los botones afectaba en el resultado

--- part1/unicafe/App.js
+++ part1/unicafe/App.js
@@ -1,6 +1,23 @@
-// ejercicio 1.7
+// ejercicio 1.8
 import React, { useState } from 'react'
-
+const Statistics  = (props) =>{
+  
+    console.log(props)
+    if(props.click.all === 0) return 'there is no stats'
+     
+    return(
+    <div>
+       <h1> Statistics </h1>,
+        <p> Good: {props.click.good} </p>
+        <p> Neutral: {props.click.neutral} </p>
+        <p> Bad: {props.click.bad} </p>
+        <p> All: {props.click.all} </p>
+        <p> Avg: {props.click.avg} </p>
+        <p> Pos: {props.click.pos} </p>
+     
+      </div>
+    ) 
+  }
 const App = () => {
   // save clicks of each button to its own state
   
@@ -17,7 +34,7 @@ const App = () => {
     good: click.good + 1,
     all: click.all + 1,
     avg: (click.good - click.bad + 1)/(click.all + 1),
-    pos: ((click.good/(click.all + 1))*100)+'%'
+    pos: (((click.good + 1)/(click.all + 1))*100)+'%'
   })
   const onClickNeutral = () => setClick({
     ...click,
@@ -34,23 +51,14 @@ const App = () => {
     pos: ((click.good/(click.all + 1))*100)+'%'
   })
   
-  const Display  = ({text, count}) =>{
-    return <p> {text} : {count} </p>
-  }
+  
   return (
     <div>
       <h1> give feedback </h1>
       <button onClick={() => onClickGood()}> Good </button>
       <button onClick={() => onClickNeutral()}> Neutral </button>
       <button onClick={() => onClickBad()}> Bad </button>
-      
-      <h1> Statistics </h1> 
-      <Display text='Good' count={click.good} />
-      <Display text='Neutral' count={click.neutral} />
-      <Display text='Bad' count={click.bad} />
-      <Display text='All' count={click.all}  />
-      <Display text='Avg' count={click.avg} />
-      <Display text='Pos' count={click.pos} /> 
+      < Statistics click={click} />
     </div>
   )
 }
